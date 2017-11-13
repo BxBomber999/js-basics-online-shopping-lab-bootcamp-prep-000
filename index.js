e4cf1202b136ca4b449267021961d42c8a540436
Peter Coyle <coyle.peters@gmail.com>
2017-11-13 20:23:06 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -75,7 +75,7 @@ function removeFromCart(item) {
 
 function placeOrder(cardNumber) {
   var tot = total()
-  if(cardNumber !=== undefined){
+  if(cardNumber !== undefined){
     console.console.log(`Your total cost is $${tot}, which will be charged to the card ${cardNumber}`);
   } else {
     console.log("Sorry, we don't have a credit card on file for you.");
